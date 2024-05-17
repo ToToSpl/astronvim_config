dd9e9b5fb3d8fafcbb740143e87560482ecfc45b
Jacek Grzybowski <jacek.grzybowski@codequest.com>
2024-05-17 15:35:27 +0200

    disable prettierd (eslint it is using it anyway)

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -15,7 +15,7 @@ return {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formatting.prettierd,
+      -- null_ls.builtins.formatting.prettierd,
       require "none-ls.diagnostics.eslint_d",
       require "none-ls.formatting.eslint_d",
     }
