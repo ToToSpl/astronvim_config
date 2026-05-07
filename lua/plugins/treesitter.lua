6411a366fcf47303bf3f8aff0d16dd5edd040e9a
Jacek Grzybowski <jacek@quantum-quest.tech>
2026-05-07 12:49:32 +0200

    updated to v6

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,28 @@
 -- Customize Treesitter
+-- --------------------
+-- Treesitter customizations are handled with AstroCore
+-- as nvim-treesitter simply provides a download utility for parsers
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
   opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "typescript",
-      -- add more arguments for adding more treesitter parsers
+    treesitter = {
+      highlight = true, -- enable/disable treesitter based highlighting
+      indent = true, -- enable/disable treesitter based indentation
+      auto_install = true, -- enable/disable automatic installation of detected languages
+      ensure_installed = {
+        "lua",
+        "vim",
+        "http",
+        "css",
+        "python",
+        "c",
+        "typescript",
+        "rust",
+        -- add more arguments for adding more treesitter parsers
+      },
     },
   },
 }
